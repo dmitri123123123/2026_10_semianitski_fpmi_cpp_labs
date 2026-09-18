7b0e423424b76ea33c97520540ee8d8ed5283868
dmitri123123123 <dmitrijsemenickij0@gmail.com>
2026-09-18 11:14:43 +0300

    Add files via upload

---
+++ laba.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+int main() {
+	std::cout << "Enter n: ";
+	long long n;
+	std::cin >> n;
+	if (n <= 0) {
+		std::cout << " ENTER ONLY POSITIVE n";
+	}
+	long long temp = n;
+	long long min_digit = 9;
+	long long lenght = 0;
+	long long powers_of_10 = 1;
+		while (temp > 0) {
+		long long current_digit = temp % 10;
+		if (current_digit != 0 && current_digit < min_digit) {
+			min_digit = current_digit;
+
+		}
+		powers_of_10 *= 10;
+		temp /= 10;
+	}
+	n = n * 10 + min_digit;
+	n = min_digit * powers_of_10 * 10 + n;
+	std::cout << n << std::endl;
+
+	return 0;
+
+
+
+
+
+}
